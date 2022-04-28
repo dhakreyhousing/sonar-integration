8a973602856e93d66833c47df165499d219067f6
Himanshu Dhakrey <himanshudhakrey@Kumars-MacBook-Pro.local>
2022-04-28 19:00:41 +0530

    first commit

---
+++ src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/SonarqubeIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.dhakreyh.sonarqube.sonarqubeintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dhakreyh/sonarqube/sonarqubeintegration/SonarqubeIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhakreyh.sonarqube.sonarqubeintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
