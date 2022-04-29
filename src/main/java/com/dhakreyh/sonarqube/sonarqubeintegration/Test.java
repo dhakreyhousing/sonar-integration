51ade942301d3c6ee755bb76c63e0191910321e0
Himanshu Dhakrey <himanshudhakrey@Kumars-MacBook-Pro.local>
2022-04-29 19:18:17 +0530

    Added Test Class

---
+++ src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test.java
@@ -0,0 +1,9 @@
+package com.dhakreyh.sonarqube.sonarqubeintegration;
+
+public class Test {
+    public static void main(String[] args) {
+        if(1 == 1){
+            System.out.println("Testing");
+        }
+    }
+}
