1ef1e0da84706ac0a1efba1e55e9d9a1ef587bd8
Himanshu Dhakrey <himanshudhakrey@Kumars-MacBook-Pro.local>
2022-05-02 15:35:22 +0530

    Fixed Sonar Issue

--- src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test.java
+++ src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test.java
@@ -2,8 +2,8 @@ package com.dhakreyh.sonarqube.sonarqubeintegration;
 
 public class Test {
     public static void main(String[] args) {
-        if(1 == 1){
+
             System.out.println("Testing");
-        }
+
     }
 }
