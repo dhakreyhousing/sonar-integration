8e4d4e5f12b7b701d1e9bb83ada2738716793a2f
Himanshu Dhakrey <himanshudhakrey@Kumars-MacBook-Pro.local>
2022-05-04 13:21:11 +0530

    adding 1 equal 1

--- src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test.java
+++ src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test.java
@@ -3,7 +3,10 @@ package com.dhakreyh.sonarqube.sonarqubeintegration;
 public class Test {
     public static void main(String[] args) {
 
+        if(1==1){
             System.out.println("Testing");
+        }
+
 
     }
 }
