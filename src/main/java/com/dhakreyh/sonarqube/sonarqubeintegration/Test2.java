352ad5dd7a7c3ad403c5550a70c35146526ce897
Himanshu Dhakrey <himanshudhakrey@Kumars-MacBook-Pro.local>
2022-05-06 11:52:50 +0530

    new java class test2

---
+++ src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test2.java
@@ -0,0 +1,8 @@
+package com.dhakreyh.sonarqube.sonarqubeintegration;
+
+public class Test2 {
+    public static void main(String[] args) {
+        if(2 ==2)
+            System.out.println("Test2");
+    }
+}
