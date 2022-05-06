d57953faa28b82d6597d8d96b0859c733e826deb
Himanshu Dhakrey <himanshudhakrey@Kumars-MacBook-Pro.local>
2022-05-06 12:09:22 +0530

    dummy

--- src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test2.java
+++ src/main/java/com/dhakreyh/sonarqube/sonarqubeintegration/Test2.java
@@ -4,5 +4,8 @@ public class Test2 {
     public static void main(String[] args) {
         if(2 ==2)
             System.out.println("Test2");
+
+        if(3 ==3)
+            System.out.println("Test2");
     }
 }
